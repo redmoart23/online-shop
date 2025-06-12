dd12c8374ec81436cea7054c773aa8c8d3c1225c
red <redmoart@gmail.com>
2025-06-11 22:17:25 -0500

    ✨feat: add authentication routing, layout, and pages for login and registration

---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,10 @@
+import {  Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [RouterLink],
+  templateUrl: './login-page.component.html',
+  styles: ``,
+})
+export class LoginPageComponent { }
